5ba9bd9e8f85027c1d74e7fd11048a43f036fb34
raziaramzan <raziaramzan075@gmail.com>
2023-08-11 22:39:57 +0500

    Arrayconcept

---
+++ store numbers with array.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier:GPL-3.0
+
+pragma solidity >=0.5.0 < 0.9.0;
+
+contract addN0WithArray{
+    uint public No;
+    uint[] public numbers;
+
+    function addNo(uint _No)public {
+    numbers.push(_No);
+    }
+    function lengthOfArray()public view returns (uint){
+        return numbers.length;
+    }
+    function remove(uint index)public {
+        delete numbers[index];
+    }
+}
+
+    
